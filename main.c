10c7c9a7440a190821061859f8e116146f4984f9
Vanille-N <neven.villani@gmail.com>
2020-12-11 17:56:36 +0100

    wrote main

---
+++ main.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+void invalid (const char* msg, int retcode) {
+    printf(msg);
+    printf("usage: mwc [-l|-w|-c] FILE\n");
+    printf("  count (l)ines, (w)ords, (c)haracters in FILE (default -w)\n");
+    exit(retcode);
+}
+
+int main (int argc, char** argv) {
+    if (argc == 1) invalid("Not enough arguments\n", WR_ARG);
+    char* file = NULL;
+    char mode = 0;
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') { // treat as an argument
+            if (mode != 0) invalid("Too many optional arguments\n", WR_OPT); // already have an option
+            switch (argv[i][1]) {
+                case 'w': case 'c': case 'l':
+                    mode = argv[i][1];
+                    break;
+                default: invalid("Invalid optional argument\n", WR_OPT); // unknown option: not w/l/c
+            }
+            if (argv[i][2] != 0) invalid("Optional argument too long\n", WR_OPT); // option is not -w/-l/-c
+        } else {
+            if (file != NULL) invalid("Too many positional arguments\n", WR_ARG);
+            file = argv[i];
+        }
+    }
+    if (file == NULL) invalid("Not enough positional arguments\n", WR_ARG);
+    if (mode == 0) mode = 'w'; // default is -w
+    printf("Treat %s with mode %c\n", file, mode);
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WR_OPT 1
+#define WR_ARG 2
+#define WR_FILE 3
+
+#endif

df60c832e0ba1f75884005cec89e54c71517166e
Vanille-N <neven.villani@gmail.com>
2020-12-12 14:36:56 +0100

    test concluded that 5 is the best number of threads

--- benches/bench.sh
+++ benches/bench.sh
@@ -3,7 +3,8 @@
 input='benches/rnd.1G.plain'
 # execs=( 'wc' './mwc-handout' './mwc-10k5' ) # measure handout vs v1.0
 # execs=( 'wc' './mwc-10k5' './mwc-10k5-dispatch' ) # measure pre- vs post-dispatch
-execs=( 'wc' './mwc-10k5' './mwc-10k5-dispatch' './mwc-30k5-dispatch' ) # measure effect of BUFSIZE
+# execs=( 'wc' './mwc-10k5' './mwc-10k5-dispatch' './mwc-30k5-dispatch' ) # measure effect of BUFSIZE
+execs=( 'wc' './mwc-30k3-dispatch' './mwc-30k4-dispatch' './mwc-30k5-dispatch' './mwc-30k6-dispatch' ) # measure effect of NB_THREADS
 
 hyperfine --warmup 2 "${execs[@]/%/ -w ${input}}"
 # hyperfine "wc -l $f" "./target -l $f" "./mwc -l $f"

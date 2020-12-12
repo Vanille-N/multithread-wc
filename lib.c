3a2043ae4908ecdd46fe4f716216969dd605ff43
Vanille-N <neven.villani@gmail.com>
2020-12-12 07:44:08 +0100

    distribute threads

--- lib.c
+++ lib.c
@@ -44,3 +44,23 @@ void* lines_in_zone (void* data) {
     pthread_exit(NULL);
 }
 
+int count_lines (const char* file) {
+    // open to determine length -> distribution of ranges
+    int fd = open(file, O_RDONLY, 0444);
+    int length = lseek(fd, 0, SEEK_END);
+    close(fd);
+
+    // launch threads
+    for (int i = 0; i < NB_THREADS; i++) {
+        zones[i].file = file;
+        zones[i].start = (i * length) / NB_THREADS;
+        zones[i].end = ((i+1) * length) / NB_THREADS;
+        pthread_create(&counters[i], NULL, lines_in_zone, (void*)&zones[i]);
+    }
+
+    // terminate threads and calculate sum
+    int sum = 0;
+    for (int i = 0; i < NB_THREADS; i++) pthread_join(counters[i], NULL);
+    for (int i = 0; i < NB_THREADS; i++) sum += zones[i].count;
+    return sum;
+}

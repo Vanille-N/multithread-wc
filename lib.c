3c627b71b6f6654934ea7205fb9589abda7c0d36
Vanille-N <neven.villani@gmail.com>
2020-12-12 07:43:59 +0100

    line counters are easy

--- lib.c
+++ lib.c
@@ -2,5 +2,45 @@
 
 int count_bytes (const char* file) {
     int fd = open(file, O_RDONLY, 0444);
-    return lseek(fd, 0, SEEK_END);
+    int pos = lseek(fd, 0, SEEK_END);
+    close(fd);
+    return pos;
 }
+
+// tells a thread what he should do
+typedef struct {
+    const char* file; // what file to read
+    int start; // where to start reading
+    int end; // where to end
+    int count; // return value = count
+} zone_t;
+
+zone_t zones [NB_THREADS];
+pthread_t counters [NB_THREADS];
+
+// A line is any number of characters followed by a '\n'
+void* lines_in_zone (void* data) {
+    // open file and init variables
+    zone_t* zone = (zone_t*)data;
+    int fd = open(zone->file, O_RDONLY, 0444);
+    lseek(fd, zone->start, SEEK_CUR);
+    char buf [BUFSIZE];
+    int length = zone->end - zone->start;
+    zone->count = 0;
+
+    // count by BUFSIZE intervals
+    while (length > 0) {
+        read(fd, buf, BUFSIZE);
+        // printf("<<%s>>", buf);
+        int iter = BUFSIZE < length ? BUFSIZE : length;
+        for (int i = 0; i < iter; i++) {
+            if (buf[i] == '\n') zone->count++;
+        }
+        length -= BUFSIZE;
+    }
+
+    // terminate
+    close(fd);
+    pthread_exit(NULL);
+}
+
--- lib.h
+++ lib.h
@@ -1,9 +1,15 @@
 #ifndef LIB_H
 #define LIB_H
 
+#define NB_THREADS 5
+#define BUFSIZE 10000
+
 #include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include <stdio.h>
 
 int count_bytes (const char* file);
 
+int count_lines (const char* file);
 #endif

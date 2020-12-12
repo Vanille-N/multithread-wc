0bb4f09788483ea0f62654cde55ee26eef060faa
Vanille-N <neven.villani@gmail.com>
2020-12-12 08:34:28 +0100

    add warmup and take a picture

--- benches/bench.sh
+++ benches/bench.sh
@@ -2,6 +2,6 @@
 
 f='benches/rnd.plain'
 
-hyperfine "wc -w $f" "./target -w $f" "./mwc -w $f"
+hyperfine --warmup 5 "wc -w $f" "./target -w $f" "./mwc -w $f"
 # hyperfine "wc -l $f" "./target -l $f" "./mwc -l $f"
 # hyperfine "wc -c $f" "./target -c $f" "./mwc -c $f"

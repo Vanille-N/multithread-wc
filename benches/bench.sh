e45577449df92f70c80ff693682cd95b22109d6a
Vanille-N <neven.villani@gmail.com>
2021-01-14 08:22:05 +0100

    update bench

--- benches/bench.sh
+++ benches/bench.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-input='benches/rnd.1G.plain'
-execs=( './mwc-handout' './mwc' ) # measure handout vs v1.0
+size="100M"
+input="benches/rnd.$size.plain"
+
+# execs=( './mwc-handout' './mwc') # measure handout vs v1.0
+execs=( './mwc-handout' './mwc-mcc' ) # measure handout vs home-compiled
 # execs=( 'wc' './mwc-10k5' './mwc-10k5-dispatch' ) # measure pre- vs post-dispatch
 # execs=( 'wc' './mwc-10k5' './mwc-10k5-dispatch' './mwc-30k5-dispatch' ) # measure effect of BUFSIZE
 # execs=( './mwc-60k1' './mwc-60k2' './mwc-60k3' './mwc-60k4' './mwc-60k5' './mwc-60k6' './mwc-60k7' './mwc-60k8' ) # measure effect of NB_THREADS
@@ -9,6 +12,9 @@ execs=( './mwc-handout' './mwc' ) # measure handout vs v1.0
 #execs=( 'wc' './mwc-read-then-check' './mwc' ) # measure effect of less overlap
 #execs=( './mwc-30k5' './mwc-60k5' ) # adjust buffer size
 
-hyperfine --warmup 2 "${execs[@]/%/ -w ${input}}"
-# hyperfine --warmup 2 "${execs[@]/%/ -l ${input}}"
-# hyperfine --warmup 2 "${execs[@]/%/ -c ${input}}"
+for arg in w l c; do
+    echo "#### MEASURE $arg ####"
+    hyperfine --warmup 2 "${execs[@]/%/ -$arg $input}"
+    echo "######################"
+    echo ""
+done
